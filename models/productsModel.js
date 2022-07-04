ad20f4786b60781baf8c098226ca85c84049ffee
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-04 16:17:46 -0300

    Chore: (Models) func getAll - adiciona verificação para retorno do banco de dados, caso esteja vazio retorna 'null'

--- models/productsModel.js
+++ models/productsModel.js
@@ -4,6 +4,8 @@ const getAll = async () => {
   const query = 'SELECT * FROM StoreManager.products ';
   const [result] = await connection.execute(query);
 
+  if (result.length === 0) return null;
+
   return result;
 };
 
