90a4d11bd4fbe1c69fc5ec012fa2e619848c5ad1
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-04 16:42:43 -0300

    chore: (Models) - O modelo 'getById' consulta o bando de dados usando o 'id' recebido por parametro.Se exister retorna um objeto com o id e nome do produto, caso não exista retorna null

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,7 +1,7 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const query = 'SELECT * FROM StoreManager.products ';
+  const query = 'SELECT * FROM StoreManager.products;';
   const [result] = await connection.execute(query);
 
   if (result.length === 0) return null;
@@ -9,6 +9,21 @@ const getAll = async () => {
   return result;
 };
 
+const getById = async (idp) => {
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ?;';
+  
+  const [result] = await connection.execute(query, [idp]);
+
+  if (result.length === 0) return null;
+
+  const { id, name } = result[0];
+  return {
+    id, 
+    name,
+  };
+};
+
 module.exports = {
   getAll,
+  getById,
 };
