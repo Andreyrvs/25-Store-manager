d7f459e735b6facc553c4ce213472456d4128fc4
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-04 19:17:53 -0300

    chore: (Tests) Controllers: getAll, getById - Testa quando não existe produtos no banco é retornado o status '404' com a mensagem .json 'Product not found'. Testa se ao receber os produtos retorna o status '200' com o .json da lista de produtos ou do id especifico

---
+++ tests/unit/controllers/productsControllers.test.js
@@ -0,0 +1,130 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const productsController = require('../../../controllers/productsController')
+const productsService = require('../../../services/productsService')
+
+describe('Ao chamar o controller getAll', function () {
+  describe('quando não existem produtos no banco de dados', function () {
+    const response = {};
+    const request = {};
+
+    before(function () {
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(productsService, 'getAll').resolves(null);
+    });
+
+    after(function () {
+      productsService.getAll.restore();
+    });
+
+    it('é chamado o método "status" 404', async function () {
+      await productsController.getAll(request, response);
+
+      expect(response.status.calledWith(404)).to.be.equal(true);
+    });
+
+    it('é chamado o método "json" com a mensagem "Product not found"', async function () {
+      await productsController.getAll(request, response);
+
+      expect(response.json.calledWith({ message: 'Product not found' })).to.be.equal(true);
+    });
+  });
+
+  describe('quando tem produtos no banco de dados', function () {
+    const response = {};
+    const request = {};
+    before(function () {
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(productsService, 'getAll').resolves(true);
+    });
+
+    after(function () {
+      productsService.getAll.restore();
+    });
+
+    it('é chamado o método "status" 200', async function () {
+      await productsController.getAll(request, response);
+
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+
+    it('é chamado o método "json" com a mensagem "Product not found"', async function () {
+      await productsController.getAll(request, response);
+
+      expect(response).to.be.an('object');
+    });
+  });
+})
+
+
+describe('Ao chamar o controller de getById', function () {
+  describe('quando não existem produtos no banco de dados', function () { 
+    const response = {};
+    const request = {};
+    
+    before(function () {
+      request.params = {
+        id: 1,
+      };
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(productsService, 'getById').resolves(null);
+    });
+
+    after(function () {
+      productsService.getById.restore();
+    });
+
+    it('é chamado o método "status" 404', async function () {
+      await productsController.getById(request, response);
+
+      expect(response.status.calledWith(404)).to.be.equal(true);
+    });
+
+    it('é chamado o método "json" com a mensagem "Product not found"', async function () {
+      await productsController.getById(request, response);
+
+      expect(response.json.calledWith({ message: 'Product not found' })).to.be.equal(true);
+    });
+  });
+
+  describe('quando tem produtos no banco de dados', function () {
+    const response = {};
+    const request = {};
+    before(function () {
+      request.params = {
+        id: 1,
+      };
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(productsService, 'getById').resolves(true);
+    });
+
+    after(function () {
+      productsService.getById.restore();
+    });
+
+    it('é chamado o método "status" 200', async function () {
+      await productsController.getById(request, response);
+
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+
+    it('é chamado o método "json" com a mensagem "Product not found"', async function () {
+      await productsController.getById(request, response);
+
+      expect(response).to.be.an('object');
+    });
+  });
+})
