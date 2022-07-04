a4f437c6c98d5fafedc3ca2be76dac2d23502f5f
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-04 16:47:42 -0300

    chore: (Controllers) - 'getById' captura o id passado por parametro e envia para camada de Service. Caso o Service não encontre o protuto o Controller retorna um verbo http 404 com uma mensagem json 'Product not found'

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -9,6 +9,19 @@ const getAll = async (_req, res) => {
   return res.status(200).json(result);
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  const result = await productsService.getById(id);
+
+  if (!result) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+
+  return res.status(200).json(result);
+};
+
 module.exports = {
   getAll,
+  getById,
 };
