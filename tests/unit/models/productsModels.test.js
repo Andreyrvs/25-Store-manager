7562d62c91ebfb8bd741f4995fe14a18d9943dbe
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-05 17:50:52 -0300

    fix: (Tests) Models - Corrige erro quando testa se o produto buscado retorna vazio/null. Retira (before, after) desnecessariro do describe: (Busca apenas um produto no BD pelo ID)

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -11,114 +11,105 @@ const allProducts = [
   { id: 3, name: 'Escudo do Capitão América' },
 ]
 
+describe('Ao chamar a camada Models', function () {
 
-describe('Retorna todos os produtos', function () {
-  
-  describe('Quando não tem a lista de produtos', function () {
-    before(async function () {
-      const execute = [[]];
-  
-      sinon.stub(connection, 'execute').resolves(execute)
-    })
-  
-    after(async function () {
-      connection.execute.restore()
-    })
-    it('retorna null', async function () {
-      const response = await productsModel.getAll();
-
-      expect(response).to.be.equal(null)
-    })
-  })
-
-  describe('Quanto tem os produtos na list', function () {
-    before(async function () {
-      sinon.stub(connection, 'execute').resolves([allProducts])
-    })
-    after(async function () {
-      connection.execute.restore()
-    })
-    describe('Busca um produto com sucesso', function () {
-      it('retorna um array', async function () {
-        const response = await productsModel.getAll();
-
-        expect(response).to.be.an('array')
-      })
-
-      it('o array não está vazio', async function () {
-        const response = await productsModel.getAll();
-
-
-        expect(response).to.be.an('array').that.not.empty
-      })
-
-      it('o array tem as propriedades: "id", "name"', async function () {
-        const response = await productsModel.getAll();
-
-        expect(response).to.deep.equals(allProducts)
-      })
-    })
-
-  })
-})
-
-describe('Busca apenas um produto no BD pelo ID', function () {
-  const id = 1
-  describe('Quando não tem um produto com o ID informado', function () {
-    before(async function () {
-      const execute = [[]];
-  
-      sinon.stub(connection, 'execute').resolves(execute)
-    })
-  
-    after(async function () {
-      connection.execute.restore()
-    })
-
-    it('retorna null', async function () {
-      const response = await productsModel.getById(id);
-
-      expect(response).to.be.null
-    })
-
-    describe('Quando existe um produto com o ID informado', function () {
+  describe('Retorna todos os produtos', function () {
+    
+    describe('Quando não tem a lista de produtos', function () {
       before(async function () {
-        const execute = {
-          id: 1,
-          name: "Martelo de Thor"
-        }
-        // No stub se eu coloco a funcao passa tudo Falso positovo?
-        sinon.stub(productsModel, 'getById').resolves(execute)
+        const execute = [[]];
+      
+        sinon.stub(connection, 'execute').resolves(execute)
       })
-
+    
       after(async function () {
-        // connection.execute.restore()
+        connection.execute.restore()
+      })
+      it('retorna null', async function () {
+        const response = await productsModel.getAll();
 
-         productsModel.getById.restore()
+        expect(response).to.be.equal(null)
+      })
+    })
+
+    describe('Quanto tem os produtos na lista', function () {
+     
+      before(async function () {
+        sinon.stub(connection, 'execute').resolves([allProducts])
+      })
+      after(async function () {
+        connection.execute.restore()
       })
 
       describe('Busca um produto com sucesso', function () {
-        it('retorna um objeto', async function () {
-          const response = await productsModel.getById(id);
+        it('retorna um array', async function () {
+          const response = await productsModel.getAll();
 
-          expect(response).to.be.an('object')
+          expect(response).to.be.an('array')
         })
 
-        it('o objeto não está vazio', async function () {
-          const response = await productsModel.getById(id);
+        it('o array não está vazio', async function () {
+          const response = await productsModel.getAll();
 
-          expect(response).to.be.not.empty
+
+          expect(response).to.be.an('array').that.not.empty
         })
 
-        it('o objeto tem as propriedades: "id", "name"', async function () {
-          const response = await productsModel.getById(id);
+        it('o array tem as propriedades: "id", "name"', async function () {
+          const response = await productsModel.getAll();
 
-          expect(response).to.have.all.keys('id', 'name')
+          expect(response).to.deep.equals(allProducts)
         })
       })
 
     })
   })
 
-});
+  describe('Busca apenas um produto no BD pelo ID', function () {
+    
+    const id = 1
 
+    describe('Quando não existe um  produto com o ID informado', function () {
+      before(async function () {
+        const execute = [[]];
+      
+        sinon.stub(connection, 'execute').resolves([execute])
+      })
+    
+      after(async function () {
+        connection.execute.restore()
+      })
+
+      it('retorna null', async function () {
+        const response = await productsModel.getById(id);
+        console.log('test', response);
+        expect(response).to.deep.equal({ id: undefined, name: undefined })
+      })
+
+      describe('Quando existe um produto com o ID informado', function () {
+
+        describe('Busca um produto com sucesso passando o "ID"', function () {
+          it('retorna um objeto', async function () {
+            const response = await productsModel.getById(id);
+
+            expect(response).to.be.an('object')
+          })
+
+          it('o objeto não está vazio', async function () {
+            const response = await productsModel.getById(id);
+
+            expect(response).to.be.not.empty
+          })
+
+          it('o objeto tem as propriedades: "id", "name"', async function () {
+            const response = await productsModel.getById(id);
+
+            expect(response).to.have.all.keys('id', 'name')
+          })
+        })
+
+      })
+    })
+
+  });
+});
