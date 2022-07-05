a0472e9316b6dcf98c5fd981eccb37b4b66afe4b
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-05 15:49:28 -0300

    Feat: (Aplicação) - utiliza o body-parser para poder ler o json que vem do corpo da requisição. Na rota '/products' utiliza o verbo http 'POST' para criar os produtos

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const rescue = require('express-rescue');
+const bodyParser = require('body-parser');
 
 const productsController = require('./controllers/productsController');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 console.log('Iniciando o projeto usando docker');
 
@@ -13,8 +16,9 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/products', rescue(productsController.getAll));
-
 app.get('/products/:id', rescue(productsController.getById));
+
+app.post('/products', rescue(productsController.create));
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
