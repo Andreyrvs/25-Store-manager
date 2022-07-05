5af5ccc24b2d40709c9174ee70593e1a07b2a404
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-05 18:28:04 -0300

    fix: (Tests) Service - Corrige erro quando testa se o produto buscado retorna vazio/null. Retira (before, after) desnecessariro do describe: (Busca apenas um produto no BD pelo ID)

--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -11,111 +11,105 @@ const allProducts = [
   { id: 3, name: 'Escudo do Capitão América' },
 ]
 
+describe('Ao chamar a camada Models', function () {
 
-describe('Retorna todos os produtos', function () {
-  before(async function () {
-    const execute = [[]];
+  describe('Retorna todos os produtos', function () {
 
-    sinon.stub(connection, 'execute').resolves(execute)
-  })
-
-  after(async function () {
-    productsService.getAll.restore()
-  })
-
-  describe('Quando não tem a lista de produtos', function () {
-    it('retorna null', async function () {
-      const response = await productsService.getAll();
-
-      expect(response).to.be.equal(null)
-    })
-  })
-
-  describe('Quanto tem os produtos na list', function () {
-    before(async function () {
-      sinon.stub(productsService, 'getAll').resolves(allProducts)
-    })
-    after(async function () {
-      connection.execute.restore()
-    })
-    describe('Busca um produto com sucesso', function () {
-      it('retorna um array', async function () {
-        const response = await productsService.getAll();
-
-        expect(response).to.be.an('array')
-      })
-
-      it('o array não está vazio', async function () {
-        const response = await productsService.getAll();
-
-
-        expect(response).to.be.an('array').that.not.empty
-      })
-
-      it('o array tem as propriedades: "id", "name"', async function () {
-        const response = await productsService.getAll();
-
-        expect(response).to.deep.equals(allProducts)
-      })
-    })
-
-  })
-})
-
-describe('Busca apenas um produto no BD pelo ID', function () {
-  const id = 1
-  describe('Quando não tem um produto com o ID informado', function () {
-    before(async function () {
-      const execute = [[]];
-  
-      sinon.stub(connection, 'execute').resolves(execute)
-    })
-  
-    after(async function () {
-      productsService.getById.restore()
-    })
-    it('retorna null', async function () {
-      const response = await productsService.getById(id);
-
-      expect(response).to.be.equal(null)
-    })
-
-    describe('Quando existe um produto com o ID informado', function () {
+    describe('Quando não tem a lista de produtos', function () {
       before(async function () {
-        const execute = {
-          id: 1,
-          name: "Martelo de Thor"
-        }
+        const execute = [[]];
 
-        sinon.stub(productsService, 'getById').resolves(execute)
+        sinon.stub(connection, 'execute').resolves(execute)
+      })
+
+      after(async function () {
+        connection.execute.restore()
+      })
+      it('retorna null', async function () {
+        const response = await productsService.getAll();
+
+        expect(response).to.be.equal(null)
+      })
+    })
+
+    describe('Quanto tem os produtos na lista', function () {
+
+      before(async function () {
+        sinon.stub(connection, 'execute').resolves([allProducts])
+      })
+      after(async function () {
+        connection.execute.restore()
+      })
+
+      describe('Busca um produto com sucesso', function () {
+        it('retorna um array', async function () {
+          const response = await productsService.getAll();
+
+          expect(response).to.be.an('array')
+        })
+
+        it('o array não está vazio', async function () {
+          const response = await productsService.getAll();
+
+
+          expect(response).to.be.an('array').that.not.empty
+        })
+
+        it('o array tem as propriedades: "id", "name"', async function () {
+          const response = await productsService.getAll();
+
+          expect(response).to.deep.equals(allProducts)
+        })
+      })
+
+    })
+  })
+
+  describe('Busca apenas um produto no BD pelo ID', function () {
+
+    const id = 1
+
+    describe('Quando não existe um  produto com o ID informado', function () {
+      before(async function () {
+        const execute = [[]];
+
+        sinon.stub(connection, 'execute').resolves([execute])
       })
 
       after(async function () {
         connection.execute.restore()
       })
 
-      describe('Busca um produto com sucesso', function () {
-        it('retorna um objeto', async function () {
-          const response = await productsService.getById(id);
-
-          expect(response).to.be.an('object')
-        })
-
-        it('o objeto não está vazio', async function () {
-          const response = await productsService.getById(id);
-
-          expect(response).to.be.not.empty
-        })
-
-        it('o objeto tem as propriedades: "id", "name"', async function () {
-          const response = await productsService.getById(id);
-
-          expect(response).to.have.all.keys('id', 'name')
-        })
+      it('retorna null', async function () {
+        const response = await productsService.getById(id);
+        console.log('test', response);
+        expect(response).to.deep.equal({ id: undefined, name: undefined })
       })
 
+      describe('Quando existe um produto com o ID informado', function () {
+
+        describe('Busca um produto com sucesso passando o "ID"', function () {
+          it('retorna um objeto', async function () {
+            const response = await productsService.getById(id);
+
+            expect(response).to.be.an('object')
+          })
+
+          it('o objeto não está vazio', async function () {
+            const response = await productsService.getById(id);
+
+            expect(response).to.be.not.empty
+          })
+
+          it('o objeto tem as propriedades: "id", "name"', async function () {
+            const response = await productsService.getById(id);
+
+            expect(response).to.have.all.keys('id', 'name')
+          })
+        })
+
+      })
     })
-  })
 
+  });
 });
-
