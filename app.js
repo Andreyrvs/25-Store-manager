85f5a025b13fe04933dbca6e8086e05c46700fed
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-08 22:24:46 -0300

    chore: (Aplicação) - cria rota '/products/:id' com verbo http 'PUT'

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ app.get('/', (_request, response) => {
 app.get('/products', rescue(productsController.getAll));
 app.get('/products/:id', rescue(productsController.getById));
 app.post('/products', rescue(productsController.create));
+app.put('/products/:id', rescue(productsController.updateById));
 
 app.post('/sales', rescue(productsController.createSale));
 app.get('/sales', rescue(salesController.getAll));
 app.get('/sales/:id', rescue(salesController.getById));
-
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
