e5bada3870a85ee298731b5ea426704a602f1a22
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-08 22:39:23 -0300

    chore: (Services) -

--- services/productsService.js
+++ services/productsService.js
@@ -1,5 +1,35 @@
 const productsModel = require('../models/productsModel');
 
+const createIsValid = (name) => {
+  if (typeof name !== 'string') return false;
+
+  return true;
+};
+
+const create = async (name) => {
+  const isNameValid = createIsValid(name);
+
+  if (!isNameValid) return null;
+
+  const result = await productsModel.create(name);
+
+  return result;
+};
+
+const createSale = async (dataSales) => {
+  const newSale = await Promise.all(dataSales.map((item) => productsModel.getById(item.productId)));
+
+  const isValidSale = newSale.some((item) => item === null);
+
+  if (isValidSale) return null;
+
+  const result = await productsModel.createSale(dataSales);
+
+  if (!result) return null;
+
+  return result;
+};
+
 const getAll = async () => {
   const result = await productsModel.getAll();
 
@@ -17,39 +47,18 @@ const getById = async (id) => {
   return result;
 };
 
-const isValid = (name) => {
-  if (typeof name !== 'string') return false;
-  
-  return true;
-};
-
-const create = async (name) => {
-  const isNameValid = isValid(name);
-
-  if (!isNameValid) return null;
-  
-  const result = await productsModel.create(name);
-
-  return result;
-};
-
-const createSale = async (dataSales) => {
-  const newSale = await Promise.all(dataSales.map((item) => productsModel.getById(item.productId)));
-  
-  const isValidSale = newSale.some((item) => item === null);
-
-  if (isValidSale) return null; 
-
-  const result = await productsModel.createSale(dataSales);
+const updateById = async (id, name) => {
+  const result = await productsModel.updateById(id, name);
 
   if (!result) return null;
-  
+
   return result;
 };
 
 module.exports = {
-  getAll,
-  getById,
   create,
   createSale,
+  getAll,
+  getById,
+  updateById,
 };
