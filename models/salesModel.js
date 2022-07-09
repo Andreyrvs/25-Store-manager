b63f02a4aa11617b0d3cdf6180a504720b0f826d
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-08 21:38:48 -0300

    chore: (Models) - Cria modelo que busca as vendas pelo 'id' recebido do Service. Caso a lista volte vazia do BD o Model retorna 'null'

--- models/salesModel.js
+++ models/salesModel.js
@@ -15,6 +15,23 @@ const getAll = async () => {
   return sales;
 };
 
+const getById = async (id) => {
+  const query = `
+    SELECT S.date, SP.product_id AS productId, SP.quantity
+    FROM ${DATABASE}.sales_products AS SP
+    INNER JOIN ${DATABASE}.sales as S
+    ON SP.sale_id = S.id
+    WHERE S.id = ?
+    ORDER BY SP.sale_id ASC, SP.product_id ASC`;
+  
+  const result = await connection.execute(query, [id]);
+
+  if (result[0].length === 0 || !result[0]) return null;
+
+  return result[0];
+};
+
 module.exports = {
   getAll,
+  getById,
 };
