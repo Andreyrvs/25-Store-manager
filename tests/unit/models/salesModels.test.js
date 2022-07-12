8fa1b5e48d3a42aa8525ef2d0c8184cc9fb72b1c
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-12 09:32:34 -0300

    fix: Aruma os testes que estavam quebrando

--- tests/unit/models/salesModels.test.js
+++ tests/unit/models/salesModels.test.js
@@ -101,14 +101,14 @@ describe('Ao chamar a camada Model para Sales', () => {
         before(async function () {
           const execute = [
             {
-              "date": "2022-07-12T11:43:56.000Z",
-              "productId": 1,
-              "quantity": 5
+              date: "2022-07-12T11:43:56.000Z",
+              productId: 1,
+              quantity: 5
             },
             {
-              "date": "2022-07-12T11:43:56.000Z",
-              "productId": 2,
-              "quantity": 10
+              date: "2022-07-12T11:43:56.000Z",
+              productId: 2,
+              quantity: 10
             }
           ]
 
@@ -132,11 +132,11 @@ describe('Ao chamar a camada Model para Sales', () => {
             expect(response).to.have.lengthOf(2);
           })
 
-          // it('o objeto tem as propriedades: "productId", "quantity", "date"', async function () {
-          //   const response = await salesModel.getById(id);
-          //   const deepKeys = [{date:1, productId:1, quantity:1}]
-          //   expect(response).to.have.all.deep.keys([{ date: 1, productId: 1, quantity: 1 }, { date: 1, productId: 1, quantity: 1 }])
-          // })
+          it('o objeto tem as propriedades: "productId", "quantity", "date"', async function () {
+            const response = await salesModel.getById(id);
+            const deepKeys = [{date:1, productId:1, quantity:1}]
+            expect(response).to.have.all.deep.keys(deepKeys)
+          })
         })
 
       })
