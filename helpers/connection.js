3096f4616e46ac5985e35afa3782d71683f09835
Andrey Visniewski <cortaesai@gmail.com>
2023-02-23 10:57:31 -0300

    fix: (Credenciais) - puxa corretamente do process.env as variaveis de ambiente do .env

--- helpers/connection.js
+++ helpers/connection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 require('dotenv').config(); // não se esqueça de configurar suas variáveis de ambiente aqui na configuração
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQL_DATABASE || 'StoreManager',
 }); 
 
