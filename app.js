b07e70126864e8b7bf0cda2632642c788646d819
Andrey Visniewski <cortaesai@gmail.com>
2023-02-23 18:37:13 -0300

    fix: (APP) - A documentação esta na rota correta

--- app.js
+++ app.js
@@ -16,11 +16,8 @@ app.use(bodyParser.json());
 
 console.log('Iniciando o projeto usando docker');
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// app.get('/', (_request, response) => {
-//   response.send();
-// });
+app.use('/', swaggerUi.serve);
+app.get('/', swaggerUi.setup(swaggerDocument));
 
 app.get('/products', rescue(productsController.getAll));
 app.get('/products/:id', rescue(productsController.getById));
@@ -34,5 +31,5 @@ app.get('/sales/:id', rescue(salesController.getById));
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
